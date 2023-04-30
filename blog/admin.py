6d597d8b6faf0509eb4c951cee2bafd9972f03f2
pirzer <pirzer1@yahoo.com>
2023-04-30 19:53:32 +0000

    removing poll class from blog admin, blog models, and templates account polls.html because it did not show results expected at the front end

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, Choice, Poll, Vote
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-
-# Choice, Poll, and Vote were added in line 2
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
@@ -19,46 +17,6 @@ class PostAdmin(SummernoteModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
-
-# Choice, Poll, and Vote were added below
-
-
-@admin.register(Choice)
-class CommentAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'body', 'post', 'created_on', 'approved') was removed from below
-    list_display = ('name',)
-    # list_filter = ('approved', 'created_on') orginal line changed to line below
-    # list_filter = ('created_on',) was removed from below
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
-
-
-@admin.register(Poll)
-class CommentAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'body', 'post', 'created_on', 'approved') was removed from below
-    list_display = ('name',)
-    # list_filter = ('approved', 'created_on') orginal line changed to line below
-    # list_filter = ('created_on',) was removed from below
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
-
-
-@admin.register(Vote)
-class CommentAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'body', 'post', 'created_on', 'approved') was removed from below
-    # list_filter = ('approved', 'created_on') orginal line changed to line below
-    # list_filter = ('created_on',) was removed from below
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.utils import timezone
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -46,35 +45,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-
-
-# Beginning of Polls setion below.
-
-class Choice(models.Model):
-    name = models.CharField(max_length=80)
-
-    def __str__(self):
-        return self.name
-
-
-class Poll(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    choices = models.ManyToManyField(
-        Choice, related_name='related_polls', blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Vote(models.Model):
-    poll = models.ForeignKey(
-        Poll, on_delete=models.SET_NULL, related_name="votes", null=True, blank=True)
-    choice = models.ForeignKey(
-        Choice, on_delete=models.SET_NULL, related_name="votes", null=True, blank=True)
-    timestamp = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return f"{self.poll.name} - {self.choice.name}"
-
-# Ending of Polls setion below.
