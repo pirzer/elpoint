845f16ccf439a4f6ccca2dd94ad1acd22ebfd85c
pirzer <pirzer1@yahoo.com>
2023-05-01 17:55:53 +0000

    4 errors fixed related to JS such as declaration or statement expected ',' property assigment expected

--- PollApp/admin.py
+++ PollApp/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from PollApp.models import Choice, Poll, Vote
 
-# Register your models here.
+# Register your models here, line 3 updated with User
+# line 8 added admin.site.register(User)
 
 admin.site.register(Choice)
 admin.site.register(Poll)
